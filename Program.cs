f82ac6a536d1037cdab59d8359898aa93bcbb0b2
SomovMaxim <71885497+SomovMaxim@users.noreply.github.com>
2020-10-23 00:24:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4workSomov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, min;
+            Console.Write("Введите x:");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Введите y:");
+            y = double.Parse(Console.ReadLine());
+            Console.Write("Введите z:");
+            z = double.Parse(Console.ReadLine());
+            min = x;
+            if (y < min)
+            {
+                min = y;
+
+            }
+            if (z < min)
+            {
+                min = z;
+            }
+            Console.WriteLine(min);
+            Console.ReadKey();
+        }
+    }
+}
